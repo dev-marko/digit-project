23696a96210977edaa882ad7bef5c250ce04ed19
dev-marko <markospasenovski00@gmail.com>
2022-12-14 23:39:34 +0100

    Deleted comments and unused imports.

--- src/components/PopularOrdersSlider.jsx
+++ src/components/PopularOrdersSlider.jsx
@@ -1,34 +1,10 @@
-import { Box, Image, Heading, Text, HStack, Stack, Flex } from '@chakra-ui/react';
+import { Box } from '@chakra-ui/react';
 import Slider from "react-slick";
 import { ArrowBackIcon, ArrowForwardIcon } from '@chakra-ui/icons';
-
-import PrimaryButton from './PrimaryButton';
 
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import PopularOrderCard from './PopularOrderCard';
-
-// function SampleNextArrow(props) {
-//   const { className, style, onClick } = props;
-//   return (
-//     <div
-//       className={className}
-//       style={{ ...style, display: "block", background: "red" }}
-//       onClick={onClick}
-//     />
-//   );
-// }
-
-// function SamplePrevArrow(props) {
-//   const { className, style, onClick } = props;
-//   return (
-//     <div
-//       className={className}
-//       style={{ ...style, display: "block", background: "green" }}
-//       onClick={onClick}
-//     />
-//   );
-// }
 
 function SampleNextArrow(props) {
   const { className, style, onClick } = props;
