37fc4793a1a76ea69f40a18d4e790a012e570a5c
dev-marko <markospasenovski00@gmail.com>
2022-12-15 17:49:56 +0100

    Code clean up

--- src/pages/AboutPage.jsx
+++ src/pages/AboutPage.jsx
@@ -1,8 +1,6 @@
 import { Container, VStack, Flex, Heading, Text, Box, Stack } from "@chakra-ui/layout";
 import { Image } from "@chakra-ui/image";
 
-import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet'
-
 const AboutPage = () => {
     return (
         <VStack bg="background.200">
@@ -50,19 +48,8 @@ const AboutPage = () => {
                     </Box>
                     <Box ms='20rem'>
                         <Heading>Location</Heading>
-                        {/* <MapContainer center={[51.505, -0.09]} zoom={13} scrollWheelZoom={false}>
-                            <TileLayer
-                                attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-                                url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-                            />
-                            <Marker position={[51.505, -0.09]}>
-                                <Popup>
-                                    A pretty CSS3 popup. <br /> Easily customizable.
-                                </Popup>
-                            </Marker>
-                        </MapContainer> */}
                         <Box mt='4'>
-                            <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d11859.356690774566!2d21.39479!3d42.003727!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0xd885169619e42643!2sAxeltra!5e0!3m2!1sen!2smk!4v1671122530442!5m2!1sen!2smk" width="640" height="260" style={{border: 0,}} allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+                            <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d11859.356690774566!2d21.39479!3d42.003727!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0xd885169619e42643!2sAxeltra!5e0!3m2!1sen!2smk!4v1671122530442!5m2!1sen!2smk" width="640" height="260" style={{ border: 0, }} allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
                         </Box>
                     </Box>
                 </Flex>
