7fce0a1a36fe3074dfbcb3504fe2d738649a8715
vaskopavic <vaskopavic@gmail.com>
2022-12-16 12:44:27 +0100

    Implemented responsive design

--- src/pages/AboutPage.jsx
+++ src/pages/AboutPage.jsx
@@ -5,11 +5,11 @@ const AboutPage = () => {
     return (
         <VStack bg="background.200">
             <Container maxW="container.xl" p="5" mt='8'>
-                <Flex flexDirection={'column'} gap='4'>
+                <Flex flexDirection={'column'} gap='4' pb={4}>
                     <Heading>
                         OUR STORY
                     </Heading>
-                    <Text fontSize={'lg'}>
+                    <Text textAlign={"justify"} fontSize={'lg'}>
                         Lorem ipsum dolor sit amet, consectetur adipiscing elit,
                         sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
                         Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi
@@ -20,11 +20,11 @@ const AboutPage = () => {
                     </Text>
                     <Flex justify={'center'}>
                         <Box size={{base: 'sm', md: 'lg'}}>
-                            <Image src='./images/about_us.jpg' borderRadius={'10px'}/>
+                            <Image w={"100vw"} src='./images/about_us.jpg' borderRadius={'10px'}/>
                         </Box>
                     </Flex>
                 </Flex>
-                <Flex flexDirection={{base: 'column', md: 'row'}} mt='8' justify={'space-between'} gap='4'>
+                <Flex flexDirection={{base: 'column', md: 'row'}} mt='8' pb='20' justify={'space-between'} gap='4'>
                     <Box>
                         <Heading>CONTACT</Heading>
                         <Stack mt='4' gap='4'>
@@ -48,8 +48,8 @@ const AboutPage = () => {
                     </Box>
                     <Box>
                         <Heading>LOCATION</Heading>
-                        <Box mt='4'>
-                            <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d11859.356690774566!2d21.39479!3d42.003727!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0xd885169619e42643!2sAxeltra!5e0!3m2!1sen!2smk!4v1671122530442!5m2!1sen!2smk" width="640" height="260" style={{ border: 0, }} allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+                        <Box width={{base: 'xs', sm: 'sm', md: 'md', lg: "2xl", xl: "4xl"}} height="260" mt='4'>
+                            <iframe width="100%" height="100%" src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d11859.356690774566!2d21.39479!3d42.003727!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0xd885169619e42643!2sAxeltra!5e0!3m2!1sen!2smk!4v1671122530442!5m2!1sen!2smk" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
                         </Box>
                     </Box>
                 </Flex>
